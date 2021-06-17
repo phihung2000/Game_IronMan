4d5d120af7a967dfae5196cd9012aad290552697
Hung Phi <you@example.com>
2021-06-17 12:48:58 +0700

    game dự án nhỏ

---
+++ demogame/src/com/hung/gameobject/BulletManager.java
@@ -0,0 +1,31 @@
+
+package com.hung.gameobject;
+
+import com.hung.state.GameWorldState;
+
+
+public class BulletManager extends ParticularObjectManager {
+
+    public BulletManager(GameWorldState gameWorld) {
+        super(gameWorld);
+    }
+
+    @Override
+    public void UpdateObjects() {
+        super.UpdateObjects(); 
+        synchronized(particularObjects){
+            for(int id = 0; id < particularObjects.size(); id++){
+                
+                ParticularObject object = particularObjects.get(id);
+                
+                if(object.isObjectOutOfCameraView() || object.getState() == ParticularObject.DEATH){
+                    particularObjects.remove(id);
+                    //System.out.println("Remove");
+                }
+            }
+        }
+    }
+    
+    
+    
+}
---
+++ demogame/src/com/hung/state/State.java
@@ -0,0 +1,22 @@
+
+package com.hung.state;
+
+import com.hung.userinterface.GamePanel;
+import java.awt.image.BufferedImage;
+
+
+public abstract class State {
+    
+    protected GamePanel gamePanel;
+    
+    public State(GamePanel gamePanel) {
+       this.gamePanel = gamePanel; 
+    }
+    
+    public abstract void Update();
+    public abstract void Render();
+    public abstract BufferedImage getBufferedImage();
+    
+    public abstract void setPressedButton(int code);
+    public abstract void setReleasedButton(int code);
+}
